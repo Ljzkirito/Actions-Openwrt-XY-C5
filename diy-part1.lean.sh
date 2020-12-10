1b1c42309dc12c716e914fe4d7a59ff3b2da69bd
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2020-12-10 22:21:25 +0800

    Update diy-part1.lean.sh

--- diy-part1.lean.sh
+++ diy-part1.lean.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Change feed source
-#sed -i 's/fw876\/helloworld/Mattraks\/helloworld;dev\/control/' feeds.conf.default
+sed -i 's/fw876/Mattraks/' feeds.conf.default
 #sed -i 's/coolsnowwolf\/packages/openwrt\/packages/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
