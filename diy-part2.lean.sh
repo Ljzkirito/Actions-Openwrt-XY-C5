07723c6df10acaf22fc08144b9b2bc1da66e222c
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2020-12-14 11:34:01 +0800

    Update diy-part2.lean.sh

--- diy-part2.lean.sh
+++ diy-part2.lean.sh
@@ -13,7 +13,7 @@
 # 修改openwrt登陆地址
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 # 版本号里显示一个自己的名字
-sed -i "s/OpenWrt/Ljzkirito build $(TZ=UTC-8 date "+%y.%m.%d")/g" package/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt/Ljzkirito build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/lean/default-settings/files/zzz-default-settings
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 # 使用官方golang版本
