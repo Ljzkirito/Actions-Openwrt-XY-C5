66cee9da4e7f7095b1420a89950f3b225c8fee3d
Ljzkirito <66788052+Ljzkirito@users.noreply.github.com>
2020-12-17 11:14:28 +0800

    Update diy-part1.Lienol.sh

--- diy-part1.Lienol.sh
+++ diy-part1.Lienol.sh
@@ -13,10 +13,6 @@
 # 修改openwrt-luci为18.06分支
 #sed -i 's/17.01/18.06/g' feeds.conf.default
 
-# 使用官方ppp
-rm -rf package/network/services/ppp
-svn co https://github.com/openwrt/openwrt/trunk/package/network/services/ppp package/network/services/ppp
-
 # 拉取最新版argon主题
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
@@ -28,12 +24,6 @@ do
 	rm -rf "$a" && svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/"$a"
 done
 popd
-
-# 获取luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns package/lean/luci-app-smartdns
-
-#关机（增加关机功能）
-#git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 
 # Remove UnblockNeteaseMusicGo upx commands
 sed -i "/upx/d" package/lean/UnblockNeteaseMusicGo/Makefile || true
